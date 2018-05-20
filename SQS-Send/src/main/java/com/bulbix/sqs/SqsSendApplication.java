52e682cc494510028e78ed0c540deede356e7b0b
bulbix <bulbix@DESKTOP-OO9SJAS>
2018-05-20 14:09:31 -0500

    first commit

--- SQS-Send/src/main/java/com/bulbix/sqs/SqsSendApplication.java
+++ SQS-Send/src/main/java/com/bulbix/sqs/SqsSendApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SqsApplication {
+public class SqsSendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SqsApplication.class, args);
+		SpringApplication.run(SqsSendApplication.class, args);
 	}
 }
